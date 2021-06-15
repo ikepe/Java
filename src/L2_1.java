5fc30639d53dd5097823dab39547c37c643d37b0
ikepe <kagokakidaiishidai@gmail.com>
2021-06-15 23:22:09 +0900

    Default Changelist

---
+++ src/L2_1.java
@@ -0,0 +1,8 @@
+public class L2_1 {
+    public static void main (String[] args)
+    {
+        System.out.println("Welcome to Java!");
+        System.out.print("\\");
+        System.out.println(123);
+    }
+}
---
+++ src/L3_1.java
@@ -0,0 +1,8 @@
+public class L3_1 {
+    public static void main (String[] args)
+    {
+        int num;
+        num = 3;
+        System.out.println("num = " + num + "");
+    }
+}
